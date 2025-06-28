8873fa3d3e3908749f5ddedb7777b4780fac33ee
Nishant Mishra <nishant.28@outlook.com>
2025-06-28 10:32:43 +0530

    Initial commit

---
+++ app/src/main/java/org/nsh07/pomodoro/MainActivity.kt
@@ -0,0 +1,27 @@
+package org.nsh07.pomodoro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.nsh07.pomodoro.ui.AppScreen
+import org.nsh07.pomodoro.ui.theme.PomodoroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PomodoroTheme {
+                AppScreen()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/org/nsh07/pomodoro/ui/AppScreen.kt
@@ -0,0 +1,15 @@
+package org.nsh07.pomodoro.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AppScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier.fillMaxSize()
+    ) {
+
+    }
+}

df7aa5595df66c056f2a636b4be9441843b604d8
Nishant Mishra <nishant.28@outlook.com>
2025-08-23 11:19:59 +0530

    chore: Bump version string, update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "org.nsh07.pomodoro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

3f8d8d76390c9294f97c093d5f61fc900bd83611
Fandroid745 <dhanush41230@gmail.com>
2025-10-28 16:51:40 +0530

    Save Local Changes

--- app/src/main/java/org/nsh07/pomodoro/ui/settingsScreen/viewModel/SettingsViewModel.kt
+++ app/src/main/java/org/nsh07/pomodoro/ui/settingsScreen/viewModel/SettingsViewModel.kt
@@ -17,9 +17,7 @@
 
 package org.nsh07.pomodoro.ui.settingsScreen.viewModel
 
-import android.R.attr.value
 import android.net.Uri
-import android.util.Log
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SliderState
@@ -261,7 +259,7 @@ class SettingsViewModel(
             }
             preferenceRepository.saveStringPreference("show_clock", showClock)
             timerRepository.showClock = showClock
-            Log.d("SettingsViewModel", "saveShowClock saved=$value")
+
 
         }
     }

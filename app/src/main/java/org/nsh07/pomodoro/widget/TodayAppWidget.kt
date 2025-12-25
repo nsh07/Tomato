fbbbb36e943d8fe25120a76b03c79366146fcd7e
Nishant Mishra <nishant.28@outlook.com>
2025-12-25 20:11:09 +0530

    feat(widget): implement a basic widget

---
+++ app/src/main/java/org/nsh07/pomodoro/widget/TodayAppWidget.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2025 Nishant Mishra
+ *
+ * This file is part of Tomato - a minimalist pomodoro timer for Android.
+ *
+ * Tomato is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Tomato is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Tomato.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.nsh07.pomodoro.widget
+
+import android.content.Context
+import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
+import androidx.glance.GlanceTheme.colors
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.provideContent
+import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.text.Text
+
+class TodayAppWidget : GlanceAppWidget() {
+    override suspend fun provideGlance(
+        context: Context,
+        id: GlanceId
+    ) {
+        provideContent {
+            GlanceTheme {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = GlanceModifier.fillMaxSize().background(colors.secondaryContainer)
+                ) {
+                    Text("Hello World!")
+                }
+            }
+        }
+    }
+}

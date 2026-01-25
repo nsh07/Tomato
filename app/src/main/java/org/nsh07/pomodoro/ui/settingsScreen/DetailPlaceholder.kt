0ef800e529040acf7638d273a466c9bc4b5f6c28
Nishant Mishra <nishant.28@outlook.com>
2026-01-25 20:28:46 +0530

    feat(settings): add basic adaptive layout support

---
+++ app/src/main/java/org/nsh07/pomodoro/ui/settingsScreen/DetailPlaceholder.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2026 Nishant Mishra
+ *
+ * This file is part of Tomato - a minimalist pomodoro timer for Android.
+ *
+ * Tomato is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Tomato is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Tomato.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.nsh07.pomodoro.ui.settingsScreen
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialShapes
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.shapes
+import androidx.compose.material3.toShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import org.nsh07.pomodoro.R
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun DetailPlaceholder() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .padding(WindowInsets.safeDrawing.asPaddingValues())
+            .background(colorScheme.surface, shapes.extraLarge)
+
+    ) {
+        val transition = rememberInfiniteTransition(
+            label = "Cookie rotate"
+        )
+        val angle by transition.animateFloat(
+            initialValue = 0f,
+            targetValue = 360f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(
+                    durationMillis = 10000,
+                    easing = LinearEasing
+                ),
+                repeatMode = RepeatMode.Restart
+            ),
+            label = "Cookie animation"
+        )
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Box(contentAlignment = Alignment.Center) {
+                Spacer(
+                    Modifier
+                        .graphicsLayer { rotationZ = angle }
+                        .background(
+                            colorScheme.surfaceVariant,
+                            MaterialShapes.Cookie12Sided.toShape()
+                        )
+                        .size(128.dp)
+                )
+                Icon(
+                    painterResource(R.drawable.settings_filled),
+                    contentDescription = null,
+                    tint = colorScheme.onSurfaceVariant,
+                    modifier = Modifier
+                        .size(72.dp)
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/org/nsh07/pomodoro/ui/settingsScreen/SettingsScreen.kt
+++ app/src/main/java/org/nsh07/pomodoro/ui/settingsScreen/SettingsScreen.kt
@@ -22,9 +22,14 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.SliderState
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.navigation3.ListDetailSceneStrategy
+import androidx.compose.material3.adaptive.navigation3.rememberListDetailSceneStrategy
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
@@ -42,10 +47,15 @@ import org.nsh07.pomodoro.ui.settingsScreen.screens.SettingsMainScreen
 import org.nsh07.pomodoro.ui.settingsScreen.screens.TimerSettings
 import org.nsh07.pomodoro.ui.settingsScreen.screens.backupRestore.BackupRestoreScreen
 import org.nsh07.pomodoro.ui.settingsScreen.viewModel.SettingsViewModel
+import org.nsh07.pomodoro.ui.theme.CustomColors.topBarColors
 import org.nsh07.pomodoro.utils.onBack
+import org.nsh07.pomodoro.utils.onTopLevelNavigate
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(
+    ExperimentalMaterial3Api::class, ExperimentalMaterial3AdaptiveApi::class,
+    ExperimentalMaterial3ExpressiveApi::class
+)
 @Composable
 fun SettingsScreenRoot(
     setShowPaywall: (Boolean) -> Unit,
@@ -93,20 +103,27 @@ fun SettingsScreenRoot(
             (slideInHorizontally(initialOffsetX = { -it / 4 }) + fadeIn())
                 .togetherWith(slideOutHorizontally(targetOffsetX = { it }))
         },
+        sceneStrategy = rememberListDetailSceneStrategy(),
         entryProvider = entryProvider {
-            entry<Screen.Settings.Main> {
+            entry<Screen.Settings.Main>(
+                metadata = ListDetailSceneStrategy.listPane(
+                    detailPlaceholder = { DetailPlaceholder() }
+                )
+            ) {
                 SettingsMainScreen(
                     settingsState = settingsState,
                     contentPadding = contentPadding,
                     isPlus = isPlus,
                     onAction = viewModel::onAction,
-                    onNavigate = backStack::add,
+                    onNavigate = backStack::onTopLevelNavigate,
                     setShowPaywall = setShowPaywall,
                     modifier = modifier,
                 )
             }
 
-            entry<Screen.Settings.About> {
+            entry<Screen.Settings.About>(
+                metadata = ListDetailSceneStrategy.detailPane()
+            ) {
                 AboutScreen(
                     contentPadding = contentPadding,
                     isPlus = isPlus,
@@ -114,7 +131,9 @@ fun SettingsScreenRoot(
                 )
             }
 
-            entry<Screen.Settings.Alarm> {
+            entry<Screen.Settings.Alarm>(
+                metadata = ListDetailSceneStrategy.detailPane()
+            ) {
                 AlarmSettings(
                     settingsState = settingsState,
                     isPlus = isPlus,
@@ -126,7 +145,9 @@ fun SettingsScreenRoot(
                 )
             }
 
-            entry<Screen.Settings.Appearance> {
+            entry<Screen.Settings.Appearance>(
+                metadata = ListDetailSceneStrategy.detailPane()
+            ) {
                 AppearanceSettings(
                     settingsState = settingsState,
                     contentPadding = contentPadding,
@@ -138,7 +159,9 @@ fun SettingsScreenRoot(
                 )
             }
 
-            entry<Screen.Settings.Backup> {
+            entry<Screen.Settings.Backup>(
+                metadata = ListDetailSceneStrategy.detailPane()
+            ) {
                 BackupRestoreScreen(
                     contentPadding = contentPadding,
                     onBack = backStack::onBack,
@@ -146,7 +169,9 @@ fun SettingsScreenRoot(
                 )
             }
 
-            entry<Screen.Settings.Timer> {
+            entry<Screen.Settings.Timer>(
+                metadata = ListDetailSceneStrategy.detailPane()
+            ) {
                 TimerSettings(
                     isPlus = isPlus,
                     serviceRunning = serviceRunning,
@@ -162,6 +187,7 @@ fun SettingsScreenRoot(
                     modifier = modifier,
                 )
             }
-        }
+        },
+        modifier = Modifier.background(topBarColors.containerColor)
     )
 }

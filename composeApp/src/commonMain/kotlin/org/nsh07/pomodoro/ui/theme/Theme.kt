877e0110a895564d6d06558866e033b2073172f5
Nishant Mishra <nishant.28@outlook.com>
2026-02-08 13:00:43 +0530

    refactor(cmp): migrate theme

--- composeApp/src/commonMain/kotlin/org/nsh07/pomodoro/ui/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/org/nsh07/pomodoro/ui/theme/Theme.kt
@@ -17,27 +17,13 @@
 
 package org.nsh07.pomodoro.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.MaterialExpressiveTheme
-import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
-import com.materialkolor.dynamiccolor.ColorSpec
-import com.materialkolor.rememberDynamicColorScheme
 
-private val lightScheme = lightColorScheme(
+internal val lightScheme = lightColorScheme(
     primary = primaryLight,
     onPrimary = onPrimaryLight,
     primaryContainer = primaryContainerLight,
@@ -75,7 +61,7 @@ private val lightScheme = lightColorScheme(
     surfaceContainerHighest = surfaceContainerHighestLight,
 )
 
-private val darkScheme = darkColorScheme(
+internal val darkScheme = darkColorScheme(
     primary = primaryDark,
     onPrimary = onPrimaryDark,
     primaryContainer = primaryContainerDark,
@@ -113,54 +99,11 @@ private val darkScheme = darkColorScheme(
     surfaceContainerHighest = surfaceContainerHighestDark,
 )
 
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
-fun TomatoTheme(
+expect fun TomatoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     seedColor: Color = Color.White,
     dynamicColor: Boolean = true,
     blackTheme: Boolean = false,
     content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> darkScheme
-        else -> lightScheme
-    }
-
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
-        }
-    }
-    CustomColors.black = blackTheme && darkTheme
-
-    val dynamicColorScheme = rememberDynamicColorScheme(
-        seedColor = when (seedColor) {
-            Color.White -> colorScheme.primary
-            else -> seedColor
-        },
-        isDark = darkTheme,
-        specVersion = if (blackTheme && darkTheme) ColorSpec.SpecVersion.SPEC_2021 else ColorSpec.SpecVersion.SPEC_2025,
-        isAmoled = blackTheme && darkTheme
-    )
-
-    val scheme =
-        if (seedColor == Color.White && !(blackTheme && darkTheme)) colorScheme
-        else dynamicColorScheme
-
-    CompositionLocalProvider(LocalAppFonts provides getAppFonts()) {
-        MaterialExpressiveTheme(
-            colorScheme = scheme,
-            typography = typography(),
-            motionScheme = MotionScheme.expressive(),
-            content = content
-        )
-    }
-}
+)

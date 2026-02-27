47d305e805de4797ea1c8a2f8d4a4bb2c2f7cfc0
Nishant Mishra <nishant.28@outlook.com>
2026-02-27 20:36:20 +0530

    refactor(service): introduce ServiceHelper as interface
    to allow cross-platform implementations of ServiceHelper

--- androidApp/src/main/java/org/nsh07/pomodoro/di/modules.kt
+++ androidApp/src/main/java/org/nsh07/pomodoro/di/modules.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2026 Nishant Mishra
+ *
+ * This file is part of Tomato - a minimalist pomodoro timer for Android.
+ *
+ * Tomato is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Tomato is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Tomato.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.nsh07.pomodoro.di
 
 import android.app.NotificationManager
@@ -25,6 +42,7 @@ import org.nsh07.pomodoro.data.AppStatRepository
 import org.nsh07.pomodoro.data.PreferenceRepository
 import org.nsh07.pomodoro.data.StatRepository
 import org.nsh07.pomodoro.data.StateRepository
+import org.nsh07.pomodoro.service.AppServiceHelper
 import org.nsh07.pomodoro.service.ServiceHelper
 import org.nsh07.pomodoro.service.addTimerActions
 import org.nsh07.pomodoro.ui.settingsScreen.screens.backupRestore.viewModel.BackupRestoreViewModel
@@ -46,7 +64,7 @@ val servicesModule = module {
     single<AppStatRepository>() bind StatRepository::class
     single<AppPreferenceRepository>() bind PreferenceRepository::class
     single<StateRepository>()
-    single<ServiceHelper>()
+    single<AppServiceHelper>() bind ServiceHelper::class
 
     single { NotificationManagerCompat.from(get()) }
     single { create(::createNotificationManager) }
